f01fe522100494325758e10a7ef52f5a44d2023e
vaidehigupta19 <vaidehigupta1975@gmail.com>
2026-06-20 21:46:07 +0530

    solved pattern19

---
+++ Patterns/pattern19.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print1(int n) {
+    int iniS = 0;
+    for (int i = 0; i < n; i++){
+        //stars 
+        for (int j =1;j<=n-i;j++) {
+            cout << "*";
+        }
+
+        //spaces
+        for (int j=0;j<iniS;j++){
+            cout << " ";
+        }
+
+        //stars
+        for (int j =1;j<=n-i;j++) {
+            cout << "*";
+        }
+        iniS += 2;
+        cout << endl;
+
+    }
+
+    iniS = 2*n-2;
+    for (int i = 1; i <= n; i++){
+        //stars 
+        for (int j =1;j<=i;j++) {
+            cout << "*";
+        }
+
+        //spaces
+        for (int j=0;j<iniS;j++){
+            cout << " ";
+        }
+
+        //stars
+        for (int j =1;j<=i;j++) {
+            cout << "*";
+        }
+        iniS -= 2;
+        cout << endl;
+
+    }
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i=0;i<t;i++){
+        int n;
+        cin >> n;
+        print1(n);
+    }
+}
