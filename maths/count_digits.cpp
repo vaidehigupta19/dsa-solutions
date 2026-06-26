7e06f7355c098ecf1c2b58d0770fc6ff7742b942
vaidehigupta19 <vaidehigupta1975@gmail.com>
2026-06-26 21:24:44 +0530

    solved count_digit

---
+++ maths/count_digits.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int n) {
+    int cnt = 0;
+    while(n>0){
+        cnt=cnt+1;
+        n=n/10;
+    }
+    return cnt;
+}
+int main() {
+    int n;
+    cin >> n;
+
+    cout << "Number of digits: " << count(n);
+
+    return 0;
+}
