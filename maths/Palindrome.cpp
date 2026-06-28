180c3fc688facfbd9a0675370113b3c2076a1d3c
vaidehigupta19 <vaidehigupta1975@gmail.com>
2026-06-28 20:45:32 +0530

    solved Palindrome

---
+++ maths/Palindrome.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int revNum=0;
+    int dup = n;
+    while(n>0) {
+        int ld = n%10;
+        revNum=(revNum*10)+ld;
+        n =n / 10;
+    }
+    if (dup == revNum) cout << "true";
+    else cout << "false";
+
+}
